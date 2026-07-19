196ac226ea7b93026d104e887ee5ed8cc5e2f205
Shivansh0047 <i.shivansh0047@gmail.com>
2026-07-19 14:49:55 +0530

    Fix X-API-KEY error

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Header, HTTPException, status
 from app.config import settings\
 
-async def get_project_id(x_api_key: str = Header(..., alias="X-API_KEY")) -> str:  # ... means it is required, tells FastAPI to look for a request header called X-API-Key
+async def get_project_id(x_api_key: str = Header(..., alias="X-API-KEY")) -> str:  # ... means it is required, tells FastAPI to look for a request header called X-API-Key
     project_id = settings.api_keys.get(x_api_key)  # looks up key in API_KEYS_JSON map
     if project_id is None:
         raise HTTPException(
