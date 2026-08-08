7f4d602a67052d562242dd52460b7dbcf3913d53
Shivansh0047 <i.shivansh0047@gmail.com>
2026-08-09 02:00:35 +0530

    test: revert embeddings to HuggingFace

--- app/rag/embeddings.py
+++ app/rag/embeddings.py
@@ -1,11 +1,18 @@
 from functools import lru_cache
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from app.config import settings
 
-
+'''
 @lru_cache(maxsize=1)
 def get_embeddings() -> GoogleGenerativeAIEmbeddings:
     return GoogleGenerativeAIEmbeddings(
         model="models/gemini-embedding-001",
         google_api_key=settings.GOOGLE_API_TOKEN,
+    )
+'''
+def get_embeddings() -> HuggingFaceEndpointEmbeddings:
+    return HuggingFaceEndpointEmbeddings(
+        model="sentence-transformers/all-MiniLM-L6-v2",
+        huggingfacehub_api_token=settings.hf_token,
     )
